bf33ff0675b569897e82b010136fb5a152586058
Alexander O'Mara <me@alexomara.com>
2021-07-19 20:27:18 -0400

    More testing

--- main.js
+++ main.js
@@ -3,14 +3,32 @@
 const path = require('path');
 
 async function main() {
+	console.log("path.resolve('/'):");
+	console.log(path.resolve('/'));
+	console.log('');
+
 	console.log("path.resolve('.'):");
 	console.log(path.resolve('.'));
 	console.log('');
+
+	console.log("path.resolve('./'):");
+	console.log(path.resolve('./'));
+	console.log('');
+
+	console.log("path.resolve('.', 'file.txt'):");
+	console.log(path.resolve('.', 'file.txt'));
+	console.log('');
+
 	console.log("path.resolve('./', 'file.txt'):");
 	console.log(path.resolve('./', 'file.txt'));
 	console.log('');
+
 	console.log("path.resolve('../', 'file.txt'):");
 	console.log(path.resolve('../', 'file.txt'));
+	console.log('');
+
+	console.log("path.resolve('../dir', 'file.txt'):");
+	console.log(path.resolve('../dir', 'file.txt'));
 	console.log('');
 }
 main().catch(err => {
