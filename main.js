9ec7e2e75a04d343b27109d2f7e6e8a9167aa8e4
Alexander O'Mara <me@alexomara.com>
2021-07-19 20:22:01 -0400

    Adding resolve

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 'use strict';
 
 async function main() {
-	console.log('TODO');
+	console.log("resolve('.'):");
+	console.log(resolve('.'));
+	console.log('');
+	console.log("resolve('./', 'file.txt'):");
+	console.log(resolve('./', 'file.txt'));
+	console.log('');
+	console.log("resolve('../', 'file.txt'):");
+	console.log(resolve('../', 'file.txt'));
+	console.log('');
 }
 main().catch(err => {
 	process.exitCode = 1;
